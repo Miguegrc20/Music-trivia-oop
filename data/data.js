3903b17ac9526b5298528c3993c0772eca45f371
Miguel Garcia <migue20016@hotmail.com>
2022-07-11 13:00:37 -0500

    First commit

---
+++ data/data.js
@@ -0,0 +1,68 @@
+export const data = [
+  {
+    question:
+      "¿Cuál de los siguientes discos NO pertenece al género del Thrash Metal?",
+    choices: [
+      "Reign in Blood",
+      "Rust in Peace",
+      "Among the Living",
+      "British Steel",
+    ],
+    answer: "British Steel",
+  },
+
+  {
+    question:
+      '¿Quién fue el productor del legendario disco de Pink Floyd "The Dark Side of the Moon"?',
+    choices: ["Roger Waters", "Alan Parsons", "George Martin", "Brian Eno"],
+    answer: "Alan Parsons",
+  },
+  {
+    question: "¿Cuál de estos grupos NO es de origen europeo?",
+    choices: ["AC/DC", "Scorpions", "Rammstein", "Queen"],
+    answer: "AC/DC",
+  },
+  {
+    question: "¿Cuántos bateristas pasaron por Nirvana antes de Dave Grohl?",
+    choices: ["3", "4", "5", "6"],
+    answer: "5",
+  },
+  {
+    question: "¿En qué año murió John Bonham?",
+    choices: ["1978", "1979", "1980", "1981"],
+    answer: "1980",
+  },
+  {
+    question: "¿Cuál fue el útlimo disco de estudio de Queen?",
+    choices: ["Made in Heaven", "Innuendo", "A Kind of Magic", "The Miracle"],
+    answer: "Made in Heaven",
+  },
+  {
+    question:
+      "Nombre del cantante y compositor de la banda británica Joy Division:",
+    choices: ["Ian Curtis", "Axl Rose", "Jack White", "Johan Hegg"],
+    answer: "Ian Curtis",
+  },
+  {
+    question:
+      "Los siguientes músicos pertenecen a la comunidad LGBTQ+, EXCEPTO:",
+    choices: ["Rob Halford", "David Bowie", "Gaahl", "Thom Yorke"],
+    answer: "Thom Yorke",
+  },
+  {
+    question:
+      "¿Cuál de los siguientes guitarristas NO es mexicano de nacimiento?",
+    choices: [
+      "Alejandro Marcovich",
+      "Carlos Santana",
+      "Kiko Cibrián",
+      "Tito Fuentes",
+    ],
+    answer: "Alejandro Marcovich",
+  },
+  {
+    question: "¿Cuál es la canción más escuchada de Tool en Spotify?",
+    choices: ["Lateralus", "Schism", "Pneuma", "Wings For Marie (Pt 1)"],
+    answer: "Schism",
+  },
+];
